313b7f2e10191b632af6dc905f28652d584866c0
dawn <dawn@atguigu.com>
2022-05-20 11:17:41 +0800

    second commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -1,5 +1,7 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git Hello");
+        System.out.println("Git2 Hello");
+        System.out.println("Git3 Hello");
     }
 }
